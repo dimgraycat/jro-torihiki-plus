05c09f45d7d16b1519b81b723dc7884ad329f6e1
dimgraycat <dimgray.cat@gmail.com>
2019-04-26 15:34:08 +0900

    chore: ファイルの読み込み方を修正

--- app/scripts/loader.ts
+++ app/scripts/loader.ts
@@ -1,3 +1,18 @@
+let config = [
+  {
+    "domain": "ragnarokonline.gungho.jp",
+    "regex": /^\/gameguide\/system\/worldstorage\/(sta.html|ygg.html)/g,
+    "js": ['scripts/worldstorage_plus.js'],
+    "css": ['styles/jro_torihiki_plus.css']
+  },
+  {
+    "domain": "rotool.gungho.jp",
+    "regex": /^\/torihiki\/.*/g,
+    "js": ['scripts/torihiki_plus.js'],
+    "css": ['styles/jro_torihiki_plus.css']
+  }
+];
+
 let script = (list: string[]): void => {
   list.forEach((file) => {
     let elem = document.createElement('script');
@@ -16,9 +31,12 @@ let css = (list: string[]): void => {
   });
 }
 
-script([
-  'scripts/torihiki_plus.js',
-  'scripts/worldstorage_plus.js'
-]);
-css(['styles/jro_torihiki_plus.css']);
+let uri = new URL(location.href);
+config.forEach((data: any): void => {
+  if (uri.host === data.domain && uri.pathname.match(data.regex)) {
+    script(data.js);
+    css(data.css);
+  }
+});
+
 
